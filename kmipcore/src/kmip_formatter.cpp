983963bb6bfa0a3c049868688f1095da693f0be1
Oleksiy Lukin <oleksiy.lukin@percona.com>
2026-06-11 14:04:51 +0300

    chore(license): align kmipclient/kmipcore file headers with dual license
    - Replace legacy GPL header blocks in kmipclient and kmipcore files with
      the project dual-license notice (Apache-2.0 OR BSD-3-Clause style text
      used in this repository).
    - Preserve the Percona copyright line:
      Copyright (c) 2025 Percona LLC and/or its affiliates. All rights reserved.
    - Add missing license headers to source/header/test files in kmipclient and
      kmipcore that previously had no top-of-file license block.
    - Exclude generated/build artifacts from header insertion.

--- kmipcore/src/kmip_formatter.cpp
+++ kmipcore/src/kmip_formatter.cpp
@@ -1,3 +1,10 @@
+/* Copyright (c) 2025 Percona LLC and/or its affiliates. All rights reserved.
+ *
+ * This file is dual licensed under the terms of the Apache 2.0 License and
+ * the BSD 3-Clause License. See the LICENSE file in the root of this
+ * repository for more information.
+ */
+
 #include "kmipcore/kmip_formatter.hpp"
 
 #include "kmipcore/kmip_basics.hpp"
